fd2c8d6dbc3623d8b2e7490122551a2c410efa50
Murad <agariomadman@gmail.com>
2019-06-10 02:20:09 -0500

    Added case sensitive words

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ var question = [
     {
     type: 'list',
     name: 'sta',
-    message: 'Do you want to replace the exact word? If not this may mess up words that start with the word you want to replace, as well as periods and commas.',
+    message: 'Do you want to replace the exact word? \nIf not this may mess up words that start with the word you want to replace.',
+    choices: ['Yes', 'No']
+    },
+    {
+    type: 'list',
+    name: 'Aa',
+    message: 'Do you want to it to be case sensitive?',
     choices: ['Yes', 'No']
     },
     {
@@ -37,19 +43,36 @@ var question = [
 
 // inquirer's prompt
 inquirer.prompt(question).then(answers => {
+    var org = answers.org
+    if(answers.Aa === "No"){
+        org = answers.org.toLowerCase()
+    }
         const bruh = answers['fsent'].split(" ")
     var counts = {}
     const outpu = async() =>{
-    for(let bru of bruh){
+    for(let bruhh of bruh){
+        var bru;
+    if(answers.Aa === "No"){
+        bru = bruhh.toLowerCase()
+    } else {
+        bru = bruhh
+    }
         if(answers.sta === "No"){
-        if(bru.startsWith(answers.org)){
-            var index = bruh.indexOf(bru)
-    bruh[index] = answers.new
+        if(bru.startsWith(org)){
+            if(bru.length === org.length){
+            var index = bruh.indexOf(bruhh)
+            bruh[index] = answers.new
             output = bruh.join(" ")
+            } else {
+                var sub = bru.substr(org.length)
+                var index = bruh.indexOf(bruhh)
+                bruh[index] = answers.new + sub
+                output = bruh.join(" ")
+            }
         }
         }else {
-        if(bru === answers.org){
-            var index = bruh.indexOf(bru)
+        if(bru === org){
+            var index = bruh.indexOf(bruhh)
     bruh[index] = answers.new
             output = bruh.join(" ")
         }
